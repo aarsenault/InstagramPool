68c70d5a19ff6475457c7c85639a4b12aa221b14
Arjun Lall <lallarjun@gmail.com>
2018-10-20 13:19:01 -0400

    add home start button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route, Switch} from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
+import Home from './components/home/Home.js';
 import {Provider} from './context';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 import Header from './components/layout/Header';
-import AddContact from './components/contacts/AddContact';
-import EditContact from './components/contacts/EditContact';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 
@@ -20,10 +19,8 @@ class App extends Component {
             <Header branding="Contact Manager"/>
               <div className="container">
                 <Switch>
-                  <Route exact path="/" component={Contacts} />
+                  <Route exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
-                  <Route exact path="/contact/add" component={AddContact} />
-                  <Route exact path="/contact/edit/:id" component={EditContact} />
                   <Route component={NotFound} />
                 </Switch>
               </div>
